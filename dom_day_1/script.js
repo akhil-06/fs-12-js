aec49d1ebd19dddcb195e7cb3e3ea0d4bff84f8e
akhil-06 <svam1008@gmail.com>
2023-09-28 22:10:09 +0530

    day9

---
+++ dom_day_1/script.js
@@ -0,0 +1,20 @@
+let head = document.getElementById("head2");
+console.log(head);
+
+// let head1 = document.getElementById("heading1");
+// // console.log(head1);
+
+// let headings = document.getElementsByTagName("h1");
+// console.log(headings);   //array
+// headings[0].style.backgroundColor = "cyan";
+// headings[1].style.color = "red";
+
+// let div1 = document.getElementById("div1");
+// console.log(div1);
+
+// let h1 = document.getElementsByTagName("div");
+// console.log(h1);
+
+// div1.style.height = "200px";
+// div1.style.width = "200px";
+// div1.style.backgroundColor = "purple";
